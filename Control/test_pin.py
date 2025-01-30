be12ab019efb2393f0088acada9316be7dcc3bd3
Matin Delkhosh <bootimat@gmail.com>
2025-01-30 16:56:05 +0330

    debuger

---
+++ Control/test_pin.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT) #ENA
+
+GPIO.output(17, GPIO.HIGH)
+GPIO.output(27, GPIO.LOW)
+#GPIO.output(23, GPIO.HIGH) 
