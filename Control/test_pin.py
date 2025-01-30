94de53b4049e0acff0770349b9d48a5583c7b8f6
Matin Delkhosh <bootimat@gmail.com>
2025-01-30 17:15:07 +0330

    pin test 5

--- Control/test_pin.py
+++ Control/test_pin.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
@@ -8,8 +9,9 @@ GPIO.setup(23, GPIO.OUT) #ENA
 GPIO.output(17, GPIO.HIGH)
 GPIO.output(27, GPIO.LOW)
 pwm = GPIO.PWM(23, 1000)  # Frequency = 1kHz
+
 pwm.start(100)  # Start with 0% duty cycle
-from time import sleep
+pwm.ChangeDutyCycle(80)
 sleep(10)
 
 
