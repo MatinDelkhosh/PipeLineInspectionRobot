25f0f2440ec7ddee2cdb4f3ae21ee7e3acfbb6d5
Matin Delkhosh <bootimat@gmail.com>
2025-02-04 18:07:54 +0330

    start

--- Control/test GPIO.py
+++ Control/test GPIO.py
@@ -23,8 +23,8 @@ pwm19 = GPIO.PWM(PWM_PIN_19, 1000) # فرکانس 1000 هرتز
 # مقداردهی اولیه
 pwm5.start(0) # PWM صفر درصد برای پین 5
 GPIO.output(PIN_6, GPIO.HIGH) # مقدار 1 برای پین 6
-GPIO.output(PIN_13, GPIO.HIGH) # مقدار 0 برای پین 13
-pwm19.start(10) # PWM 10 درصد برای پین 19
+GPIO.output(PIN_13, GPIO.LOW) # مقدار 0 برای پین 13
+pwm19.start(5) # PWM 10 درصد برای پین 19
 
 try:
     while True:
